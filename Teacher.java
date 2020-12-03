8fba2b663804d22e72560b5927e3590c36050544
amandaclapper <amanda.clapper@rochesterprep.org>
2020-12-03 13:50:09 +0000

    Starter Code

---
+++ Teacher.java
@@ -0,0 +1,13 @@
+/*
+ * Name
+ */
+
+public class Teacher
+{
+    //Attributes (class/ intance variables)
+    
+    //Countructors (create one for default and one for non-default) You must instantiate all arrtibutes
+    
+    //Behaviors (Methods) You should have 2 accesor methods and 3 mutator methods
+    
+}
